6e5ae3d52ccca9402b50eecfad48339cfd7c5a63
Andrew Wilhelm <andrewwilhelmex@gmail.com>
2016-08-15 16:07:37 -0700

    Project and dependencies set up

---
+++ CasaDeCartas/CasaDeCartas.h
@@ -0,0 +1,19 @@
+//
+//  CasaDeCartas.h
+//  CasaDeCartas
+//
+//  Created by Andrew Wilhelm on 8/15/16.
+//  Copyright © 2016 Geezer, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CasaDeCartas.
+FOUNDATION_EXPORT double CasaDeCartasVersionNumber;
+
+//! Project version string for CasaDeCartas.
+FOUNDATION_EXPORT const unsigned char CasaDeCartasVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CasaDeCartas/PublicHeader.h>
+
+
